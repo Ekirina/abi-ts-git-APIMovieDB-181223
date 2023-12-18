03d60e4f7ddbc7476d3534f2b4ad45795648c8b8
Ainara <ainarabliz@gmail.com>
2023-12-18 17:57:10 +0100

    first

---
+++ apimoviedb/src/app/main/base/characters/characters.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-characters',
+  templateUrl: './characters.component.html',
+  styleUrls: ['./characters.component.css']
+})
+export class CharactersComponent{
+  actualPage:number = 1;
+
+  nextPage(){
+    if(this.actualPage<42) this.actualPage++;
+  }
+  previousPage(){
+    if(this.actualPage>1) this.actualPage--;
+  }
+  goToFirstPage(){
+    this.actualPage = 1;
+  }
+  goToLastPage(){
+    this.actualPage = 42;
+  }
+}
